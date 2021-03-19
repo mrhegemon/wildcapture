d0b47b4a3025e7a36fd3b30b64688953efd7de3b
William Broza <lbroza@bitscoop.com>
2021-03-18 17:27:07 -0700

    Video2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Header } from './components/Header';
 import { Home } from './pages/Home';
 import { WhatWeDo } from './pages/WhatWeDo';
 import { Volumetric } from './pages/Volumetric';
+import { Volliam } from './pages/Volliam';
 
 
 function Box() {
@@ -25,6 +26,7 @@ function App() {
           <Route path={'/'} exact component={Home}></Route>
           <Route path={'/whatwedo'} component={WhatWeDo}></Route>
           <Route path={'/volumetric'} component={Volumetric}></Route> 
+          <Route path={'/vol-liam-url'} component={Volliam}></Route> 
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/pages/Volliam.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import { VolumetricPlayer } from '../components/VolumetricPlayer';
+import './Volumetric.scss'
+
+export const Volliam = () => {
+  const meshFilePath = document.location.origin + "/liam2.drcs";
+  const videoFilePath = document.location.origin + "/liam2.mp4";
+    return (
+        <Fragment>
+            <section className="volumetric__wrapper" >
+              <VolumetricPlayer style={{ width: '100%', height: '100%' }} meshFilePath={meshFilePath} videoFilePath={videoFilePath} />
+            </section>
+        </Fragment>
+    )
+}
